4836d748f9c0ec066de3edd79be5ffde8b878ef3
Snehal Senapati <78613699+snehalsenapati23@users.noreply.github.com>
2023-05-19 13:33:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TodoList from './components/TodoList';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+     <TodoList />
+    </div>
+  );
+}
+
+export default App;
